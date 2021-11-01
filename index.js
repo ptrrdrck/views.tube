0c635d0ee01d8e60c0c5b3213b6277099bbd7bcb
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2021-11-01 14:34:50 -0400

    Add caption variable

--- index.js
+++ index.js
@@ -4,35 +4,40 @@ const grams = {
     "--secondaryColor": "rgba(0, 0, 0, 1)",
     "--altBgColor": "rgba(0, 0, 0, 0.05)",
     "--filterColor": "hue-rotate(0deg)",
-    "--emojiSize": "2em"
+    "--emojiSize": "2em",
+    "--caption": "'clck 2 eat'"
   },
   oneGram: {
     "--primaryColor": "rgba(0, 0, 0, 1)",
     "--secondaryColor": "rgba(255, 255, 255, 1)",
     "--altBgColor": "rgba(255, 255, 255, 0.05)",
     "--filterColor": "grayscale(100%)",
-    "--emojiSize": "3em"
+    "--emojiSize": "3em",
+    "--caption": "'clck 2 eat'"
   },
   twoGrams: {
     "--primaryColor": "rgba(195, 218, 254, 1)",
     "--secondaryColor": "rgba(60, 54, 107, 1)",
     "--altBgColor": "rgba(60, 54, 107, 0.05)",
     "--filterColor": "hue-rotate(240deg)",
-    "--emojiSize": "4em"
+    "--emojiSize": "4em",
+    "--caption": "'clck 2 eat'"
   },
   threeGrams: {
     "--primaryColor": "rgba(195, 254, 222, 1)",
     "--secondaryColor": "rgba(54, 107, 61, 1)",
     "--altBgColor": "rgba(54, 107, 61, 0.05)",
     "--filterColor": "hue-rotate(120deg)",
-    "--emojiSize": "5em"
+    "--emojiSize": "5em",
+    "--caption": "'clck 2 eat'"
   },
   fourGrams: {
     "--primaryColor": "rgba(244, 195, 254, 1)",
     "--secondaryColor": "rgba(105, 54, 107, 1)",
     "--altBgColor": "rgba(105, 54, 107, 0.05)",
     "--filterColor": "hue-rotate(300deg)",
-    "--emojiSize": "6em"
+    "--emojiSize": "6em",
+    "--caption": "'clck 4 reality'"
   }
 };
 
@@ -42,12 +47,12 @@ document.getElementById("change").addEventListener("click", function () {
   gramsIndex++;
   var gramCount = Object.keys(grams).length;
   gramsIndex = gramsIndex <= gramCount - 1 ? gramsIndex : 0;
-  var theme = grams[Object.keys(grams)[gramsIndex]];
-  activateTheme(theme);
+  var weight = grams[Object.keys(grams)[gramsIndex]];
+  eat(weight);
 });
 
-function activateTheme(theme) {
-  for (let prop in theme) {
-    document.querySelector(":root").style.setProperty(prop, theme[prop]);
+function eat(weight) {
+  for (let effect in weight) {
+    document.querySelector(":root").style.setProperty(effect, weight[effect]);
   }
 }

b96af41858bce0a468bc7efdc16352c9d80890e9
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2021-11-01 12:55:53 -0400

    Create index script (color switching)

---
+++ index.js
@@ -0,0 +1,48 @@
+const grams = {
+  noGrams: {
+    "--primaryColor": "rgba(255, 255, 255, 1)",
+    "--secondaryColor": "rgba(0, 0, 0, 1)",
+    "--altBgColor": "rgba(0, 0, 0, 0.05)",
+    "--filterColor": "hue-rotate(0deg)"
+  },
+  oneGram: {
+    "--primaryColor": "rgba(0, 0, 0, 1)",
+    "--secondaryColor": "rgba(255, 255, 255, 1)",
+    "--altBgColor": "rgba(255, 255, 255, 0.05)",
+    "--filterColor": "grayscale(100%)"
+  },
+  twoGrams: {
+    "--primaryColor": "rgba(195, 218, 254, 1)",
+    "--secondaryColor": "rgba(60, 54, 107, 1)",
+    "--altBgColor": "rgba(60, 54, 107, 0.05)",
+    "--filterColor": "hue-rotate(240deg)"
+  },
+  threeGrams: {
+    "--primaryColor": "rgba(195, 254, 222, 1)",
+    "--secondaryColor": "rgba(54, 107, 61, 1)",
+    "--altBgColor": "rgba(54, 107, 61, 0.05)",
+    "--filterColor": "hue-rotate(120deg)"
+  },
+  fourGrams: {
+    "--primaryColor": "rgba(244, 195, 254, 1)",
+    "--secondaryColor": "rgba(105, 54, 107, 1)",
+    "--altBgColor": "rgba(105, 54, 107, 0.05)",
+    "--filterColor": "hue-rotate(300deg)"
+  }
+};
+
+var gramsIndex = 0;
+
+document.getElementById("change").addEventListener("click", function () {
+  gramsIndex++;
+  var gramCount = Object.keys(grams).length;
+  gramsIndex = gramsIndex <= gramCount - 1 ? gramsIndex : 0;
+  var theme = grams[Object.keys(grams)[gramsIndex]];
+  activateTheme(theme);
+});
+
+function activateTheme(theme) {
+  for (let prop in theme) {
+    document.querySelector(":root").style.setProperty(prop, theme[prop]);
+  }
+}
